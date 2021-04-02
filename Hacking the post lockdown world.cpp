642a919a8dc7a801833b7a466d9125560bcee597
Kushal Krishna <kushal5070@gmail.com>
2021-04-02 19:46:54 +0530

    7:46pm

--- Hacking the post lockdown world.cpp
+++ Hacking the post lockdown world.cpp
@@ -2,7 +2,33 @@
 
 using namespace std;
 
-int main()
+
+void intro()
 {
+	cout<<setw(328)<<"Contact Tracing Software";	
+	cout<<setw(214)<<"by Aryan, Kushal and Shivam\n\n";
+}
+
+
+int main()
+{	
+	intro();
+	
+	int choice;
+	cout<<"Menu\n1:Customer Entry\n2:Customer Exit\n3:Report Case\n4:Exit\n";
+	cout<<"\n\nEnter your choice: ";
+	cin>>choice;
+	
+	switch(choice)
+	{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			exit(0);
+	}
 	return 0;
 }
